cc10216770334d68ed775eeac05128dacd5bf21a
Dziyana Arabei <darabei@ibagroup.eu>
2019-09-19 12:05:44 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import './app.scss';
 import { Button } from 'carbon-components-react';
+import { Content } from 'carbon-components-react/lib/components/UIShell';
+import TutorialHeader from './components/TutorialHeader';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          Hello Carbon! Well, not quite yet. This is the starting point for the
-          Carbon tutorial.
-        </div>
+      <>
+      <TutorialHeader />
+      <Content>
         <Button>Button</Button>
-      </div>
+      </Content>
+    </>
     );
   }
 }
