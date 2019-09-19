e65a5cb223b092b420dee6925143076135739f69
Arabey Diana <diana.arabey@mail.ru>
2019-09-19 12:26:06 +0300

    add routing

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Notification20 from '@carbon/icons-react/lib/notification/20';
 import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
 import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import { Link } from 'react-router-dom';
 
 import {
   Header,
@@ -15,22 +16,24 @@ import {
 const TutorialHeader = () => (
   <Header aria-label="Carbon Tutorial">
     <SkipToContent />
-    <HeaderName href="/" prefix="IBM">
+    <HeaderName element={Link} to="/" prefix="IBM">
       Carbon Tutorial
     </HeaderName>
     <HeaderNavigation aria-label="Carbon Tutorial">
-      <HeaderMenuItem href="/repos">Repositories</HeaderMenuItem>
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
     </HeaderNavigation>
-    
+
     <HeaderGlobalAction aria-label="Notifications">
-    <Notification20 />
-  </HeaderGlobalAction>
-  <HeaderGlobalAction aria-label="User Avatar">
-    <UserAvatar20 />
-  </HeaderGlobalAction>
-  <HeaderGlobalAction aria-label="App Switcher">
-    <AppSwitcher20 />
-  </HeaderGlobalAction>
+      <Notification20 />
+    </HeaderGlobalAction>
+    <HeaderGlobalAction aria-label="User Avatar">
+      <UserAvatar20 />
+    </HeaderGlobalAction>
+    <HeaderGlobalAction aria-label="App Switcher">
+      <AppSwitcher20 />
+    </HeaderGlobalAction>
     <HeaderGlobalBar />
   </Header>
 );
